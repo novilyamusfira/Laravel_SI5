33b7d3fff103fbeb7405ca096823868d10463c58
novilyamusfira <novilya.musfira@gmail.com>
2025-10-08 00:41:23 +0700

    Route Laravel

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return 'Halo Laravel';
+});
+
+Route::get('about', function () {
+    return 'Ini adalah Halaman <b>About</b>';
+});
+
+Route::get('mahasiswa', function () {
+    return 'Ini adalah Halaman <b>Mahasiswa</b>';
+});
+
+Route::get('profile', function () {
+    $nama = "fulan";
+    return 'Ini adalah Halaman <b>profile</b>, nama saya adalah' . '$nama';
 });
