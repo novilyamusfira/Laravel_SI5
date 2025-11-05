8cb5a8ed92cb68ad1c5a0ff21ad21dc87c49cbe8
novilyamusfira <novilya.musfira@gmail.com>
2025-11-05 08:08:55 +0700

    UTS - Soal 3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,7 +25,7 @@
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Produk</a>
+            <a class="nav-link" href="/produk">Produk</a>
           </li>
         </ul>
         <form class="d-flex mt-3" role="search">
--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,23 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('about', function () {
-    return view('about');
+Route::get('/about', function () {
+    $nama = 'Novilya Musfira Bahri';
+    return view('about')->with('nama',$nama);
 });
 
-Route::get('mahasiswa', function () {
+Route::get('/mahasiswa', function () {
     return view('mahasiswa');
 });
 
-Route::get('profile', function () {
-    $nama = "Novilya Musfira Bahri";
-    //return view('profile', compact('nama'));
-    return view('profile')->with('nama', $nama);
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/produk', function () {
+    $produk = [
+        ['kode' => 'BRG001', 'nama' => 'Pena', 'jenis' => 'Alat tulis', 'harga' => 20000],
+        ['kode' => 'BRG002', 'nama' => 'Buku', 'jenis' => 'Alat tulis', 'harga' => 15000],
+    ];
+    return view('produk', compact('produk'));
 });
