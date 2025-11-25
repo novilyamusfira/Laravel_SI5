b3f86d10d400670117089c42f8c7835500f98644
novilyamusfira <novilya.musfira@gmail.com>
2025-11-25 09:50:27 +0700

    Membuat Controller mahasiswaController

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,6 @@
 <?php
 
+use App\Http\Controllers\mahasiswaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/about', function () {
-    $nama = 'Novilya Musfira Bahri';
-    return view('about')->with('nama',$nama);
-});
-
-Route::get('/mahasiswa', function () {
-    return view('mahasiswa');
-});
-
-Route::get('/profile', function () {
-    return view('profile');
-});
-
-Route::get('/produk', function () {
-    $produk = [
-        ['kode' => 'BRG001', 'nama' => 'Pena', 'jenis' => 'Alat tulis', 'harga' => 20000],
-        ['kode' => 'BRG002', 'nama' => 'Buku', 'jenis' => 'Alat tulis', 'harga' => 15000],
-    ];
-    return view('produk', compact('produk'));
-});
-
-Route::get('/tambah', function () {
-    $jenis = [
-        'Alat Tulis',
-        'Elektronik',
-        'Sembako',
-    ];
-    return view('tambah', compact('jenis'));
-});
+Route::get('mahasiswa',[mahasiswaController::class,'index']);
